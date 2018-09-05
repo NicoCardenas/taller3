0b261fb2139f48763b013ed003cae9b1cd83ebe5
Nicolas Cardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2018-09-04 22:15:46 -0500

    primer push

---
+++ src/main/java/edu/eci/pdsw/models/Todo.java
@@ -0,0 +1,47 @@
+package edu.eci.pdsw.models;
+
+import java.lang.String;
+
+public class Todo {
+    int userId;
+    int id;
+    String title;
+    boolean completed;
+
+    public Todo	(){
+
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
+
