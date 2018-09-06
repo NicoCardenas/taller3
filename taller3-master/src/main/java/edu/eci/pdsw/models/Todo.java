5716d91455cd70d39af817f8f8f2b5ad29c24838
acai-bjca <42592184+acai-bjca@users.noreply.github.com>
2018-09-05 22:32:57 -0500

    Add files via upload

---
+++ taller3-master/src/main/java/edu/eci/pdsw/models/Todo.java
@@ -0,0 +1,45 @@
+package edu.eci.pdsw.models;
+
+public class Todo {
+    int userId;
+    int id;
+    String title;
+    boolean completed;
+
+    public Todo	(){
+
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
+
